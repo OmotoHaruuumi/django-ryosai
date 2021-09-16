79b24eaf116e5181e7ddc801c63941d49846aa58
omoto haruumi <omoharu@outlook.jp>
2021-09-16 20:47:24 +0900

    whitenoise test

--- ryosai/settings.py
+++ ryosai/settings.py
@@ -123,8 +123,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
