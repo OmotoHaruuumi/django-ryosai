0cec5270e80f02bed1605531e3300b01f15adc54
omoto haruumi <omoharu@outlook.jp>
2021-11-04 19:08:27 +0900

    2

--- ryosai_2021/admin.py
+++ ryosai_2021/admin.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from . import models
-from import_export import resources  
-from import_export.admin import ImportExportModelAdmin
-from .models import Pro
 
-class ProResource(resources.ModelResource):
 
-    class Meta:
-        model = Pro
-        import_id_fields = ("name","body","place","image")
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -25,5 +18,5 @@ class PostAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(models.Pro)
-class ProAdmin(ImportExportModelAdmin):
-      resource_class = ProResource
+class ProAdmin(admin.ModelAdmin):
+    pass
